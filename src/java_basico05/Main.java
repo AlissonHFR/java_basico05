e9e7f4f2f580b4bc082b7d4032aad170e80470fe
Alisson <alissonhenrique98@gmail.com>
2020-03-04 20:24:58 -0300

    Segundo commit

--- src/java_basico05/Main.java
+++ src/java_basico05/Main.java
@@ -10,7 +10,22 @@ public class Main {
 	}
 
 	private void start() {
-		// TODO Auto-generated method stub
+		
+		for(int i = 0;i<=5;i++) {
+			switch(i) {
+			case 1: System.out.println("Primeira condição");
+			break;
+			case 2: System.out.println();
+			break;
+			case 3: System.out.println();
+			break;
+			case 4: System.out.println();
+			break;
+			case 4: System.out.println();
+			break;
+			
+			}
+		}
 		
 	}
 
